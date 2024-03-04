8617c26f1398034e659a3727516081c4835b115d
Divya R <kwrdivyaraj100@gmail.com>
2024-03-04 12:20:08 +0530

    Exporting fetchUsers

--- features/user/userSlice.js
+++ features/user/userSlice.js
@@ -1,5 +1,5 @@
 const createSlice = require('@reduxjs/toolkit').createSlice
-const createAsyncThunk = require('@reduxjs/toolkit'). createAsyncThunk
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
 const axios = require('axios')
 
 
@@ -20,5 +20,27 @@ const fetchUsers = createAsyncThunk('user/fetchUsers', function () {
 });
 
 const userSlice = createSlice({
-    name: 'user'
+    name: 'user',
+    initialState: initialState,
+    extraReducers: function (builder) {
+        builder.addCase(fetchUsers.pending, function (state) {
+            state.loading = true;
+        });
+
+        builder.addCase(fetchUsers.fulfilled, function (state, action) {
+            state.loading = false;
+            state.users = action.payload;
+            state.error = '';
+        });
+
+        builder.addCase(fetchUsers.rejected, function (state, action) {
+            state.loading = false;
+            state.users = [];
+            state.error = action.error.message;
+        });
+    },
 });
+
+module.exports = userSlice.reducer
+module.exports.fetchUsers = fetchUsers
+
